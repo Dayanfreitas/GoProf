a93701859406121f3da42e005330a0ff27c55b02
dayanFreitas <dayan.freitas@catolicasc.com.br>
2023-04-28 23:40:20 -0300

    Ajuste de model de user

--- back-app/src/model/user.js
+++ back-app/src/model/user.js
@@ -1,31 +1,21 @@
-const Password = require('objection-password')({
-    passwordField: 'passwordHash'
-});
-const { Model } = require('objection');
-const jwt = require('jsonwebtoken');
+const ModelBase = require('../database/database')
 
-class User extends Password(Model) {
+class User extends ModelBase {
     static get tableName() {
         return 'users';
     }
     
-    generateToken () {
-        return jwt.sign({ id: this.id }, process.env.APP_SECRET, {
-            expiresIn: process.env.EXPIRATION_TIME*1,
-        });
-    }
-
     static get jsonSchema() {
         return {
             type: 'object',
-            required: ['name', 'email', 'passwordHash', 'phone', 'genero'],
+            required: ['name', 'last_name', 'email'],
             properties: {
                 id: { type: 'integer' },
                 name: { type: 'string', minLength: 1, maxLength: 255 },
+                last_name: { type: 'string', minLength: 1, maxLength: 255 },
                 email: { type: 'string', minLength: 1, maxLength: 255 },
-                passwordHash: { type: 'string', minLength: 8, maxLength: 255 },
-                phone: { type: 'string', minLength: 11, maxLength: 255 },
-                genero: { type: 'string', minLength: 1, maxLength: 1 },
+                token_sub_google: { type: 'string', minLength: 1, maxLength: 255 },
+                image_path: { type: 'string', minLength: 1, maxLength: 255 }
             }
         }
     }
