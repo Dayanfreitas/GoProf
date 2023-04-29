689b9faddf8905a4286b91947b49a8c18e8b7b6a
dayanFreitas <dayan.freitas@catolicasc.com.br>
2023-04-29 16:30:01 -0300

    Controller de conteúdos

--- back-app/src/controller/contents.js
+++ back-app/src/controller/contents.js
@@ -1,12 +1,21 @@
-// const { User } = require("../model");
-
+const { Content } = require("../model");
 const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  // const users = await User.query().findById(1);
-  res.status(200).json({ ok: true, contents: [] })
-});
+  const contents = await Content.query().select('id').from('contents')
+  
+  const contentsIds = contents.map(content => {
+    return content.id
+  })
+
+  res.status(200).json({ ok: true, contents: contentsIds })
+})
+
+router.get('/:id', async (req, res) => {
+  const content = await Content.query().findById(req.params.id)
+  res.status(200).json({ ok: true, content })
+})
 
 
 module.exports = router
